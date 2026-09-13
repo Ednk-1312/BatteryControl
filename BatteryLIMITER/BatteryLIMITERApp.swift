b056459da27a30d3a0ca401aa311aadb20f00ded
Eshan Nandakumar <eshan.nandakumar@icloud.com>
2026-09-13 16:56:47 -0700

    Initial Commit

---
+++ BatteryLIMITER/BatteryLIMITERApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryLIMITERApp.swift
+//  BatteryLIMITER
+//
+//  Created by Eshan Nandakumar on 9/13/26.
+//
+
+import SwiftUI
+
+@main
+struct BatteryLIMITERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
